86d09fd07e444ee134e28e60a18c2fcf46d60c35
Robin Kuo <robin.kuo.chin.ting@gmail.com>
2025-01-02 11:37:21 +0800

    load modules_dict import all modules

--- utils.py
+++ utils.py
@@ -31,10 +31,37 @@ def load_functions_from_folder(folder_path: str) -> dict[str, callable]:
                 funcs_dict[attr] = getattr(module,attr)
     return funcs_dict
 
+def load_modules_from_folder(folder_path: str) -> dict[str, str]:
+    """
+    收集 folder 目錄下所有模塊中的 modules 常量
+    """
+    modules_dict = {}
+    folder_path = os.path.dirname(os.path.abspath(__file__)) + "/api"
+    
+    # 獲取 prompt 目錄下所有 .py 文件
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.py') and not filename.startswith('__'):
+            module_name = filename[:-3]  # 移除 .py 後綴
+            try:
+                # 修改導入方式，直接使用完整的包路徑
+                module = importlib.import_module(f"api.{module_name}")                
+                # 獲取模塊中所有成員
+                members = inspect.getmembers(module)
+
+                for name, value in members:
+                    modules_dict[name] = value
+                        
+            except ImportError as e:
+                print(f"無法導入模塊 {module_name}: {str(e)}")
+                print(f"當前 Python 路徑: {os.sys.path}")
+            except Exception as e:
+                print(f"處理模塊 {module_name} 時發生錯誤: {str(e)}")
+    
+    return modules_dict
 
 def load_description_from_folder(folder_path: str) -> dict[str, str]:
     """
-    收集 prompt 目錄下所有模塊中的 XXX_DESCRIPTION 常量
+    收集 folder 目錄下所有模塊中的 XXX_DESCRIPTION 常量
     """
     description_dict = {}
     folder_path = os.path.dirname(os.path.abspath(__file__)) + "/api"
@@ -68,12 +95,18 @@ if __name__ == '__main__':
     functions_dict = load_functions_from_folder(api_path)
 
     print(f"Current api functions in {api_path}:")
-    for func_name in functions_dict:
-        if '_api' in func_name:
-            print(f"{func_name}")
+    print(functions_dict)
+    # for func_name in functions_dict:
+    #     if '_api' in func_name:
+    #         print(f"{func_name}")
     
     description_dict = load_description_from_folder(api_path)
     print(f"Current api description in {api_path}:")
-    for description_name in description_dict:
-        # if 'DESCRIPTION' in description_name:
-        print(f"{description_name}")
+    print(description_dict)
+    # for description_name in description_dict:
+    #     # if 'DESCRIPTION' in description_name:
+    #     print(f"{description_name}")
+
+    modules_dict = load_modules_from_folder(api_path)
+    print(f"Current modules in {api_path}:")
+    print(modules_dict)
