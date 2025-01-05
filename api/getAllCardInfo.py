770f7c18738f65aa614519c97132da62b4880c84
Robin Kuo <robin.kuo.chin.ting@gmail.com>
2025-01-05 08:54:46 +0800

    api getAllCardInfo: add testcase + rm clearAll testcase

--- api/getAllCardInfo.py
+++ api/getAllCardInfo.py
@@ -15,25 +15,25 @@ from api.utils import api_hit
 #from utils import api_hit
 #from addCard import run_api as addCard
 import json
-def run_api(msg: str):
-    ret = api_hit("GetAllCardInfo", msg)
+def run_api():
+    ret = api_hit("GetAllCardInfo")
     if ret == "[]":
         print(f"{api_name}: No card")
     return ret
 
 def run_testcase(input=None, expect_output=None):
-    ret = run_api(input)
-    return ret
-    #addCard("test getAllCardInfo if no card")
+    ret = run_api()
 
-    if ret == expect_output:
+    if len(ret) != 0:
         return True
+    else:
+        return None
 
 if __name__ == "__main__":
-    
     #action = "GetAllCardInfo"
     #allcardinfo = api_hit(action)
     allcardinfo = run_api()
+    print(allcardinfo)
     #print(allcardinfo)
     #print(type(allcardinfo))
     allcard = json.loads(allcardinfo)
--- tests/test_api.py
+++ tests/test_api.py
@@ -11,13 +11,14 @@ class TestAPI(unittest.TestCase):
         result = addCard.run_testcase(input=msg, expect_output=None)
         self.assertEqual(result, True)
 
-    def test_clearAllCard(self):
-        result = clearAllCard.run_testcase()
-        self.assertEqual(result, True)
+    # Commented out because it clears all cards
+    #def test_clearAllCard(self):
+    #    result = clearAllCard.run_testcase()
+    #    self.assertEqual(result, True)
 
-    #def test_getAllCardInfo(self):
-        #result = getAllCardInfo.run_testcase()
-        #self.assertEqual(result, True)
+    def test_getAllCardInfo(self):
+        result = getAllCardInfo.run_testcase()
+        self.assertEqual(result, True)
 
 if __name__ == '__main__':
     unittest.main()
