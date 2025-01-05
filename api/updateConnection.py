7dfd1c878fa6340ef642f519c6cd45f76ba0d8a6
Robin Kuo <robin.kuo.chin.ting@gmail.com>
2025-01-05 18:30:47 +0800

    api: updateConnection: fix testcase failed after clear

--- api/updateConnection.py
+++ api/updateConnection.py
@@ -13,10 +13,12 @@ prompt = '''
 try:
     from utils import api_hit
     #from getCardInfoByID import id_exist as id_exist
+    from addConnection import run_testcase as create_dummy_connection
     from getAllConnectionInfo import run_api as getAllConnectionInfo
 except:
     from api.utils import api_hit
     #from api.getCardInfoByID import id_exist as id_exist
+    from api.addConnection import run_testcase as create_dummy_connection
     from api.getAllConnectionInfo import run_api as getAllConnectionInfo
 
 def run_api(id: str, description: str):
@@ -34,11 +36,11 @@ def run_api(id: str, description: str):
 
 def run_testcase(id=None, description=None):
     if id is None and description is None: # Dummy testcase
-        id = "linkdummy0dummy1"
+        create_dummy_connection()
+        id = "linkdummy0_dummy1"
         description = "connection between dummy0 and dummy1"
 
     run_api(id=id, description=description)
-
     try:
         connection_list = getAllConnectionInfo()
         for connection in connection_list:
@@ -51,7 +53,7 @@ def run_testcase(id=None, description=None):
 if __name__ == "__main__":
     print(f"== {api_name} ==")
 
-    id = "linkdummy0dummy1"
+    id = "linkdummy0_dummy1"
     description = "connection between dummy0 and dummy1"
     print(f"Testcase result: {run_testcase(id=id, description=description)}")
 
